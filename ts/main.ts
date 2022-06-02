7f90172f1cf8a2f6a431705259d2b672b0c330c1
Thilina-Upendra <upendravass1996@gmail.com>
2022-06-02 18:08:34 +0530

    Add js, and implement index.html file and movement fix

--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,9 @@
-const pointer=document.getElementById("pointer");
+const pointer=document.getElementById("pointer")!;
 
+pointer.style.position = 'relative';
 window.addEventListener("mousemove",(e)=>{
     pointer.style.left=(e.clientX +15)+"px";
     pointer.style.top=(e.clientY+15)+"px";
 });
+
+console.log("Working");
